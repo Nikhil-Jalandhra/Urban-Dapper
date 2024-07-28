4fc96c897810ded27280e07ea270600103ecc129
Nikhil-Jalandhra <jneek36@gmail.com>
2024-07-28 14:08:32 +0530

    functionality of category items

--- src/Pages/Shop.tsx
+++ src/Pages/Shop.tsx
@@ -1,21 +1,48 @@
 import './Shop.css'
 import ProductCard from '../Components/ProductCard';
 import productData from '../Database/ProductDatabase';
+import { useEffect, useState } from 'react';
 
 function Shop() {
+
+  const [filterCategory, setFilterCategory] = useState("All");
+  const [finalProductsData, setFinalProductsData] = useState(productData);
+
+  useEffect(() => {
+    filterFunction()
+  }, [filterCategory]);
+
+
+  const  setFilterValue = (e) => {
+    setFilterCategory(e.target.value)
+  }
+
+  console.log(filterCategory)
+
+  const filterFunction = () => {
+    if(filterCategory === "All"){
+       setFinalProductsData(productData)
+      }else {
+        const data = productData.filter((item)=> (item.category === filterCategory))
+        setFinalProductsData(data)
+
+    }
+  }
+  
+
   return (
     <div>
       <div className='productCardsContainer'>
         <div className='productsFilter'>
-          <button>All</button> 
-          <button>Belts</button> 
-          <button>Jackets</button> 
-          <button>Shoes</button> 
-          <button>T-Shirts</button>
-          <button>Wallets</button> 
+          <button value="All" onClick={setFilterValue}>All</button> 
+          <button value="Belts" onClick={setFilterValue} >Belts</button> 
+          <button value="Jackets" onClick={setFilterValue} >Jackets</button> 
+          <button value="Shoes" onClick={setFilterValue} >Shoes</button> 
+          <button value="T-Shirts" onClick={setFilterValue} >T-Shirts</button>
+          <button value="Wallets" onClick={setFilterValue} >Wallets</button> 
         </div> 
         <div className="shopContainer">
-            {productData.map((item,index)=> (
+            {finalProductsData.map((item,index)=> (
                 <ProductCard key={index} item={item}/>
             ))}
         </div>
@@ -23,5 +50,6 @@ function Shop() {
     </div>
   );
 }
+
 
 export default Shop;
