cc6c052ae73e533bcebbc5238e6ebae2d132fc08
Nikhil-Jalandhra <jneek36@gmail.com>
2024-07-28 12:09:14 +0530

    create UI of shop page

--- src/Pages/Shop.tsx
+++ src/Pages/Shop.tsx
@@ -5,11 +5,21 @@ import productData from '../Database/ProductDatabase';
 function Shop() {
   return (
     <div>
+      <div className='productCardsContainer'>
+        <div className='productsFilter'>
+          <button>All</button> 
+          <button>Belts</button> 
+          <button>Jackets</button> 
+          <button>Shoes</button> 
+          <button>T-Shirts</button>
+          <button>Wallets</button> 
+        </div> 
         <div className="shopContainer">
             {productData.map((item,index)=> (
                 <ProductCard key={index} item={item}/>
             ))}
         </div>
+      </div>
     </div>
   );
 }
