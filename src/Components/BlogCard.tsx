08b34c5035e812512e03f70220d4a7fad5400134
Nikhil-Jalandhra <jneek36@gmail.com>
2024-07-31 18:08:36 +0530

    add Link for blogSummary page

--- src/Components/BlogCard.tsx
+++ src/Components/BlogCard.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import './BlogCard.css'
 
 function BlogCard({item}) {
 
   return (
     <div>
+      <Link to={`/blog/${item.id}/${item.title1}`}>
       <div className='blogCardContainer'>
         <div className='blogImageContainer'>
             <img className='blogImage' src={item.image1} alt={item.title1} />
@@ -14,6 +16,7 @@ function BlogCard({item}) {
             <p>{item.description1.slice(0,160)}...</p>
           </div>
         </div>
+      </Link>
     </div>
   );
 }
