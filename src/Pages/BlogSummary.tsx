aa1e545b8dc8ee280b8446fea5d7b37fd58f4d4d
Nikhil-Jalandhra <jneek36@gmail.com>
2024-08-23 16:47:52 +0530

    work on blogSummary component #responsivness

--- src/Pages/BlogSummary.tsx
+++ src/Pages/BlogSummary.tsx
@@ -18,9 +18,9 @@ function BlogSummary() {
                 <img className="blogHeroImage" src={blogDetails?.image1} alt={blogDetails?.title1} />
             </div>
             <div className="blogHeroDetailsContainer">
-                <p>By <span>{blogDetails?.author}</span> {blogDetails?.date}</p>
+                <p className="blogAuthour" >By <span>{blogDetails?.author}</span> {blogDetails?.date}</p>
                 <h2>{blogDetails?.title1}</h2>
-                <p>{blogDetails?.description1}</p>
+                <p className="blogParagraph">{blogDetails?.description1}</p>
                 <h2>{blogDetails?.title2}</h2>
                 <p>{blogDetails?.description2}</p>
                 <div className="blogCoStar">
@@ -38,7 +38,7 @@ function BlogSummary() {
                     <div className="blogLastLineDetails">
                         <p>At last:&nbsp;<span>MEN</span>&nbsp;WILL&nbsp; <span>be</span>&nbsp;Men&nbsp;<span><GiSpy/></span></p>
                         <div className="blogSocialIcon">
-                            <p>Follow me:</p>
+                            <h4>Follow me:</h4>
                             <p><FaGithub /></p>
                             <p><FaLinkedin /></p>
                             <p><RiTwitterXFill /></p>
