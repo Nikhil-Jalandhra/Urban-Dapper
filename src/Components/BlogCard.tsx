c11f9f8e9cde3e44255d84f9a595b9099bc947ff
Nikhil-Jalandhra <jneek36@gmail.com>
2024-08-30 20:01:08 +0530

    formating & linting of BlogCard component

--- src/Components/BlogCard.tsx
+++ src/Components/BlogCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import './BlogCard.css'
+import './BlogCard.css';
 
 interface blogCardData {
   item: { 
@@ -21,20 +21,22 @@ interface blogCardData {
 function BlogCard({item}: blogCardData) {
 
   return (
+    
     <div>
       <Link to={`/blog/${item.id}/${item.title1}`}>
-      <div className='blogCardContainer'>
-        <div className='blogImageContainer'>
-            <img className='blogImage' src={item.image1} alt={item.title1} />
-        </div>
+        <div className='blogCardContainer'>
+          <div className='blogImageContainer'>
+              <img className='blogImage' src={item.image1} alt={item.title1} />
+          </div>
           <div className='blogDetailsContainer'>
-            <p>By <span>{item.author}</span> \ {item.date}</p>
-            <h2>{item.title1.slice(0,25)}...</h2>
-            <p>{item.description1.slice(0,160)}...</p>
+              <p>By <span>{item.author}</span> \ {item.date}</p>
+              <h2>{item.title1.slice(0,25)}...</h2>
+              <p>{item.description1.slice(0,160)}...</p>
           </div>
         </div>
       </Link>
     </div>
+
   );
 }
 
