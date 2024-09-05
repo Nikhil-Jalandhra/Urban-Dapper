f7eeb1b6915f01a87ba86336df17a1c3c7701d69
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-05 21:43:01 +0530

    formating & linting of Loader component

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -3,9 +3,9 @@ import './Loader.css'
 function Loader() {
   return (
     <div>
-        <div className="loaderContainer">
-         <div className="loader"></div>      
-        </div>
+      <div className="loaderContainer">
+        <div className="loader"></div>      
+      </div>
     </div>
   );
 }
