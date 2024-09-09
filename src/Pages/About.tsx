9ed8a4d7f0d904720954ebe920a9fb1985e0e1ea
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-09 21:42:48 +0530

    formating & linting of About component

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,51 +1,34 @@
-import aboutHeroImg from "../../Public/pageImages/aboutHeroImg.jpg"
-import OurStory from "../Components/OurStory";
+import './About.css';
 import OurTeam from "../Components/OurTeam";
-import './About.css'
+import OurStory from "../Components/OurStory";
+import aboutHeroImg from "../../Public/pageImages/aboutHeroImg.jpg";
 
 function About() {
   return (
     <div>
-
       <div className="aboutImageContainer">
         <img src={aboutHeroImg} className="aboutImage" alt="" />
       </div>
 
       {/* Our story */}
-
       <OurStory/>
 
       {/* Carousel */}
-
       <div className="aboutCarouselContainer">
-
         <div className="aboutCarousel">
-          
           <div className="aboutCarouselAnimation">
-            <p>
-              URBAN-DAPPER
-            </p>
-            <p>
-              URBAN-DAPPER
-            </p>
+            <p>URBAN-DAPPER</p>
+            <p>URBAN-DAPPER</p>
           </div>
           <div className="aboutCarouselAnimation">
-            <p>
-              URBAN-DAPPER
-            </p>
-            <p>
-              URBAN-DAPPER
-            </p>
+            <p>URBAN-DAPPER</p>
+            <p>URBAN-DAPPER</p>
           </div>
-
         </div>
       </div>
 
       {/* Our team */}
-
       <OurTeam/>
-      
-
     </div>
   );
 }
