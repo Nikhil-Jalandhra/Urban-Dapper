aabc8eb598b28eedbd4663d9e0b9abce4bb23640
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-12 21:46:26 +0530

    formating & linting of Home component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,11 @@
-import "./Home.css"
+import "./Home.css";
 import { Link } from "react-router-dom";
 import OurStory from "../Components/OurStory";
+import BlogCard from "../Components/BlogCard";
 import blogData from "../Database/blogDatabase";
 import product from "../Database/ProductDatabase";
 import ProductCard from "../Components/ProductCard";
-import homeImage from "../../Public/pageImages/homeImage1.jpg"
-import BlogCard from "../Components/BlogCard";
+import homeImage from "../../Public/pageImages/homeImage1.jpg";
 
 function Home() {
 
@@ -16,45 +16,43 @@ function Home() {
     
   return (
     <div>
-
         {/* Home display image */}
-
         <div className="homeImageContainer">
-            <img src={homeImage} alt="Clothes Image" className="homeImage" />
+          <img src={homeImage} alt="Clothes Image" className="homeImage" />
         </div>
 
         {/* Home image text */}
-
         <div className="homeTextContainer">
-
             <div className="homeTextContainerHeading">
                 <h2 className="homeHeadingText">Crafted</h2>
             </div>
-
             <div className="homeTextContainerHeading2">
                 <h2 className="homeHeadingText2">for Confidence</h2>
             </div>
-
         </div>
 
         {/* Our story */}
-
         <OurStory/>
 
         <div className="prouductFeaturedCardContainer">
-            <Link to={"/shop/All"}><h1>Products</h1></Link>
+            <Link to={"/shop/All"}>
+                <h1>Products</h1>
+            </Link>
             <div className="prouductCardsContainer">
-                {uniqueItem.map((item, index) => 
-                    ( index <= 3 && <ProductCard item={item} key={index}/>))}
+                {uniqueItem.map((item, index) => ( 
+                    index <= 3 && <ProductCard item={item} key={index}/>
+                ))}
             </div>
         </div>
 
 
         <div className="blogFeaturedCardContainer">
-            <Link to={"/Blog"}><h1>Latest Blogs</h1></Link>
+            <Link to={"/Blog"}>
+                <h1>Latest Blogs</h1>
+            </Link>
             <div className="blogCardsContainer">
                 {blogData.map((item)=> (
-                    item.id <= 3 &&  <BlogCard key={item.id} item={item}/> 
+                   item.id <= 3 &&  <BlogCard key={item.id} item={item}/> 
                 ))}
             </div>
         </div>
